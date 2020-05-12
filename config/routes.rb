fb1e0694aaa8f12c9dfe45fab4b50f8087892b08
Edgar Minault <e.minault@bluewin.ch>
2020-05-12 12:56:37 +0200

    Create functionnality done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index', as: :list
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'new', to: 'tasks#new', as: :add
+  post 'tasks', to: 'tasks#create'
 end
